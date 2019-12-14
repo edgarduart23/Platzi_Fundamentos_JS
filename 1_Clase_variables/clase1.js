2b10414c098b58d70e08739aa79dd5e234eb18b1
edgardo <edgarduart2012@hotmail.com.ar>
2019-12-14 18:45:37 -0300

    first class

---
+++ 1_Clase_variables/clase1.js
@@ -0,0 +1,11 @@
+//console.log('Hola mundo JS')
+/* var nombre;
+nombre='Edgardo';
+*/
+var nombre='Edgardo', apellido= "Duarte";
+var edad=28;
+edad='28 años';
+
+console.log('Hola ' + nombre + ' ' + apellido);
+console.log('Tengo ' + edad)
+var peso= 75;
